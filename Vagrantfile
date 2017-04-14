270170b657990e2e9e58e7dacd62305f606e18d0
Maximilian Eham <maximilian-eham@gmx.net>
2017-04-14 12:49:42 +0200

    fixed mistakes in puppet manifest changed puppet module version for mayflower-php to 4.0 added link in index.html to phpinfo.php

--- Vagrantfile
+++ Vagrantfile
@@ -9,23 +9,15 @@ Vagrant.configure("2") do |config|
      vb.memory = "768"
   end
 
-  # Define a Vagrant Push strategy for pushing to Atlas. Other push strategies
-  # such as FTP and Heroku are also available. See the documentation at
-  # https://docs.vagrantup.com/v2/push/atlas.html for more information.
-  config.push.define "atlas" do |push|
-     push.app = "particle/lamp"
-  end
-
   config.vm.provision "shell", inline: <<-SCRIPT
-    apt-get update
+    apt-get update --fix-missing
     apt-get install -y puppet
     
     puppet module install puppetlabs-apache
     puppet module install puppetlabs-mysql --version 3.10.0
-    puppet module install mayflower-php --version 4.0.0-beta1
+    puppet module install mayflower-php --version 4.0
   SCRIPT
 
   config.vm.provision "puppet", run: "always"
 end
 
-# sudo puppet apply --verbose /vagrant/manifests/default.pp
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,55 +1,15 @@
-user { 'ubuntu':
-	ensure => present,
-	uid    => '1000',
-	gid    => '1000'
-}
-
-# change motd:
-file { '/etc/update-motd.d/00-header':
-	ensure  => present,
-	mode    => '755',
-	source => '/vagrant/manifests/motd/00-header'
-}
-file { '/etc/update-motd.d/01-vagrant-info':
-	ensure  => present,
-	mode    => '755',
-	source => '/vagrant/manifests/motd/01-vagrant-info'
-}
-file { '/etc/update-motd.d/10-help-text':
-	ensure => absent
-}
-file { '/etc/update-motd.d/51-cloudguest':
-	ensure => absent
-}
-
-
 class { 'apt':
+  update => {
+    frequency => 'daily',
+  },
 }
 
-class { '::php::globals':
-  php_version => '7.0',
-  # config_root => '/etc/php/7.0',
-}->
 class { '::php':
-  	composer => true,
-  	settings   => {
-	    'PHP/max_execution_time'  => '90',
-	    'PHP/max_input_time'      => '300',
-	    'PHP/memory_limit'        => '64M',
-	    'PHP/post_max_size'       => '32M',
-	    'PHP/upload_max_filesize' => '32M',
-	    'Date/date.timezone'      => 'Europe/Berlin',
-	},
-	zend_extensions => { 
-		xdebug => {
-			provider => pecl,
-		},
-	}
 }
 
 class { 'apache': 
 	default_vhost => false,
-	mpm_module => prefork,
+	mpm_module => prefork, # needed for apache mod_php
 }
 apache::vhost { 'localhost':  # define vhost resource
  	port       => '80',
